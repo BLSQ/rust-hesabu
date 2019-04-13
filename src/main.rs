587d6d53fd373740ba3e039d7eb35546f88e40c4
mestachs <mestachs>
2019-04-13 19:16:41 +0200

    POC solver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use evalexpr::build_operator_tree;
+
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut problem = [["c", "a + 10 * b"], ["a", "10"], ["b", "10+a"]];
+
+    for key_value in &problem {
+        let key = key_value[0];
+        let equation = key_value[1];
+        println!("PROCESSING : {} => {}", key, equation);
+
+        if let Ok(tree) = build_operator_tree(equation) {
+            for identifier in tree.iter_identifiers() {
+                println!("{}", identifier)
+            }
+            // ...
+        }
+    }
 }
