9fccf7b0a6552b627e10cb7d857e1b4b6360df79
mestachs <mestachs>
2019-04-14 09:56:46 +0200

    Fake toposort and eval in order

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,55 @@
 use evalexpr::build_operator_tree;
+use evalexpr::Context;
+use evalexpr::HashMapContext;
+use evalexpr::Value;
 
 use std::collections::HashMap;
 
 fn main() {
-    let mut problem = [
+    let problem = [
         ["c", "a + 10 * b"],
         ["a", "10"],
         ["b", "10+a"],
-        ["d", "a + sin(b)"],
+        ["d", "a + (b)"],
+        //   ["err", "sin(a"],
     ];
+
+    let mut parsed_equations = HashMap::new(); //<String,Node>()
 
     for key_value in &problem {
         let key = key_value[0];
         let equation = key_value[1];
         println!("PROCESSING : {} => {}", key, equation);
+        let node = build_operator_tree(equation);
+        match node {
+            Ok(tree) => {
+                parsed_equations.insert(key, tree);
 
-        if let Ok(tree) = build_operator_tree(equation) {
-            for identifier in tree.iter_identifiers() {
-                println!("{}", identifier)
+                for identifier in parsed_equations.get(&key).unwrap().iter_identifiers() {
+                    println!("{}", identifier)
+                }
+                // ...
             }
-            // ...
+            Err(error) => println!("{}", error),
+        }
+    }
+
+    let toposort = ["a", "b", "c", "d"];
+
+    let mut context = HashMapContext::new();
+
+    for identitier in &toposort {
+        let equation = parsed_equations.get(identitier);
+
+        match equation.unwrap().eval_with_context(&context) {
+            Ok(value) => {
+                println!("{}", &value);
+                context.set_value(identitier.to_string(), value).unwrap();
+            }
+            Err(error) => {
+                println!("{}", error);
+                panic!(error);
+            }
         }
     }
 }
